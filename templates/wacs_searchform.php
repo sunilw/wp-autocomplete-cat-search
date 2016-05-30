e8c4132453d4547a9b33d819d6005968b5516118
Sunil Williams <sunil@sunil.co.nz>
2016-05-30 12:19:39 +1200

    structure in place

---
+++ templates/wacs_searchform.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * The actual search form
+ */
+
+?>
+
+<h2>searchform</h2>
+<form role="search" method="get" id="searchform" class="searchform" action="http://www.test.dev/">
+    <div>
+        <label class="screen-reader-text" for="s">Search for:</label>
+        <input type="text" value="" name="s" id="s" />
+        <input type="submit" id="searchsubmit" value="Search" />
+    </div>
+</form>
--- wp-autocomplete-cat-search.php
+++ wp-autocomplete-cat-search.php
@@ -15,4 +15,20 @@ License: GPLv2 or later
 */
 defined( 'ABSPATH' ) or die( 'naff off' );
 
+define( "WPACS_PLUGINDIRURI",  plugin_dir_url(__FILE__) ) ; 
+define( "WPACS_PLUGINDIR",  plugin_dir_path(__FILE__) ) ;
+/*
+*  present the search form
+*/
+
+add_action('', '') ;
+function wpacs_publish_searchform() {
+    ob_start() ;
+    ?>
+    <h2>In function. About to output form...</h2>
+    <?php 
+    include(  WPACS_PLUGINDIR   . '/templates/wacs_searchform.php') ;
+    return ob_get_clean() ;
+}
+add_shortcode('wpac-cat-search', 'wpacs_publish_searchform'  ) ;
 
