b3a5bf43d316bf0496eab8c89a14e7e49dc75bf9
Sunil Williams <sunil@sunil.co.nz>
2016-05-30 12:01:02 +1200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Autocomplete Category Search
+==========================
+
+Publish a search form that has autocomplete for provided categories.
+This is not a standalone product. It's bound to a custom theme.
+
+
---
+++ wp-autocomplete-cat-search.php
@@ -0,0 +1,18 @@
+<?php 
+
+/*
+Plugin Name: Autocomplete Category Search
+Plugin URI: 
+Description: Publish a form that affords a category search. Form autocompletes
+Version: 0.1.0
+Author:  Sunil Williams
+Author URI: http://sunil.co.nz
+License: GPLv2 or later
+*/
+
+/*
+* don't allow direct access
+*/
+defined( 'ABSPATH' ) or die( 'naff off' );
+
+
